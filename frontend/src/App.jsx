9bc6b2424c9f60d7e193d1cb5f19fe242a850a36
Kapil287905 <kmeghnani28@gmail.com>
2025-06-19 20:38:03 +0530

    intership layput change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,58 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import DepartmentList from './components/DepartmentList'
 import DepartmentForm from './components/DepartmentForm'
+import './index.css'
+import { toggleSidebar } from './script.js'
 
 function App() {
   return (
-    <div className="container mt-5">
-      <h1 className="text-center mb-4">Department Management</h1>
-      <nav className="mb-4">
-        <Link to="/" className="btn btn-outline-primary me-2">View Departments</Link>
-        <Link to="/add" className="btn btn-primary">Add Department</Link>
-      </nav>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">Admin Dashboard</Link>
-          <div>
-            <Link className="nav-link d-inline" to="/">Departments</Link>
-            <Link className="nav-link d-inline" to="/add">Add New</Link>
-          </div>
+    <>
+    <nav>
+        <div className="logo-name">  
+            <span className="logo_name">Logo</span>
         </div>
-      </nav>
 
+        <div className="menu-items">
+            <ul className="nav-links" style={{paddingLeft:"0rem"}}>
+                <li><a href="/">
+                    <i className="uil uil-estate"></i>
+                    <span className="link-name">View Departments</span>
+                </a></li>
+                <li><a href="/add">
+                    <i className="uil uil-files-landscapes"></i>
+                    <span className="link-name">Add Department</span>
+                </a></li>
+                
+            </ul>
+            
+            <ul className="logout-mode">                
+              <li>
+                <div className="mode-toggle">
+                </div>
+              </li>
+            </ul>  
+        </div>
+    </nav>
+    <section className="dashboard">
+      <div className="top">
+           <i className="uil uil-bars sidebar-toggle"  onClick={toggleSidebar}></i>
+                      
+      </div>
+      <div className="dash-content">
+      <div class="overview">
+      <div class="title">
+          <i class="uil uil-tachometer-fast-alt"></i>
+          <span class="text">Department Management</span>
+      </div>
       <Routes>
         <Route path="/" element={<DepartmentList />} />
         <Route path="/add" element={<DepartmentForm />} />
         <Route path="/edit/:id" element={<DepartmentForm />} />
       </Routes>
-    </div>
+      </div>
+      </div>
+    </section>
+    </>
   )
 }
 
